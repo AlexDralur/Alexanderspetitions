c8642dee99bc343ccf9678daf4adc03c78483767
alexanderalves <aalexanderalves@gmail.com>
2025-11-08 17:55:57 +0000

    Update HomeController file to prevent whitelabel bug

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -1,4 +1,4 @@
-package controller;
+package org.example.alexanderspetitions.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
