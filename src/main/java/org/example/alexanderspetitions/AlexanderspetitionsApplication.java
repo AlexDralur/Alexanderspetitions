9c6acb99c417bbac299ea0042c5c1839d79b62c2
alexanderalves <aalexanderalves@gmail.com>
2025-11-09 17:10:18 +0000

    Create further basic pages and add CreateServlet

--- src/main/java/org/example/alexanderspetitions/AlexanderspetitionsApplication.java
+++ src/main/java/org/example/alexanderspetitions/AlexanderspetitionsApplication.java
@@ -2,8 +2,10 @@ package org.example.alexanderspetitions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class AlexanderspetitionsApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/example/alexanderspetitions/servlet/CreateServlet.java
+++ src/main/java/org/example/alexanderspetitions/servlet/CreateServlet.java
@@ -1,4 +1,29 @@
 package org.example.alexanderspetitions.servlet;
 
-public class CreateServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "CreateServlet", urlPatterns = "/create-petition")
+public class CreateServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+
+        // Check if the print is being made
+        System.out.println("Petition created:");
+        System.out.println("Title: " + title);
+        System.out.println("Description: " + description);
+
+        // Return to main page
+        response.sendRedirect("/");
+    }
 }
